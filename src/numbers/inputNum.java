f532afa1e236a9ce6e3d7444dd0a1eeac4bbc300
SrPollo25 <ortegachkn25@gmail.com>
2021-12-21 19:54:31 +0100

    Made numbers and fixed the package name

--- src/numbers/inputNum.java
+++ src/numbers/inputNum.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Numbers;
+package numbers;
 
 import java.util.Scanner;
 
@@ -12,6 +12,12 @@ import java.util.Scanner;
  */
 public class inputNum {
 
+    /**
+     * Ask for a number
+     * ask for the user for a number 
+     * @param message choose the message for asking the number
+     * @return return the number
+     */
     public int inputNumInt(String message) {
         Scanner input = new Scanner(System.in);
         int number;
@@ -20,7 +26,13 @@ public class inputNum {
         return number;
     }
 
-    public int inputNumIntPos(String message, int min) {
+    /**
+     * Ask for a number grater than min
+     * @param message choose the message for asking the number
+     * @param min choose de minimum number for the asking number
+     * @return return the asked number
+     */
+    public int inputNumIntGrater(String message, int min) {
         Scanner input = new Scanner(System.in);
         int number;
         System.out.println(message);
@@ -33,7 +45,15 @@ public class inputNum {
         }
         return number;
     }
-    public int inputNumIntPos(String message, int min, int max) {
+    
+    /**
+     * Ask for a number between a min and max
+     * @param message choose the message for asking the number
+     * @param min choose the minimum number for the asking number
+     * @param max choose the maximum number for the asking number
+     * @return return the asked number
+     */
+    public int inputNumIntBetween(String message, int min, int max) {
         Scanner input = new Scanner(System.in);
         int number;
         System.out.println(message);
