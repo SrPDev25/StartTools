00c3fb0b4977f426a4d0e83c818bbbaf534d2586
SrPollo25 <daniel.ortega.alu@santamaria-artesano.org>
2022-01-17 10:15:17 +0100

    Creado un método que calcula la fecha de caducidad

--- src/objects/Fecha.java
+++ src/objects/Fecha.java
@@ -12,9 +12,10 @@ public class Fecha {
 
     private int fecha[] = new int[3];
     private static int diasMeses[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};//static ya que pertenece la misma lista a todas las fechas
-
+    private static final String[] MESES={"Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"};
     /**
      * Constructor con una fecha ya introducida
+     *
      * @param inputFecha String de una fecha con formato dd/mm/aaaa
      */
     public Fecha(String inputFecha) {
@@ -22,7 +23,7 @@ public class Fecha {
     }
 
     /**
-     * 
+     *
      */
     public Fecha() {
     }
@@ -32,7 +33,7 @@ public class Fecha {
      * Método para pedir que se introduzca la fecha
      */
     public void inputFecha() {
-     Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         String fechaProvisional;
         System.out.print("Introduce la fecha (dd/mm/aaaa): ");
         fechaProvisional = input.nextLine();
@@ -40,16 +41,15 @@ public class Fecha {
             System.out.println("Error: Se introdujo mal la fecha");
             System.out.print("Introduce la fecha (dd/mm/aaaa): ");
             fechaProvisional = input.nextLine();
-        }   
+        }
     }
 
-    
-    
-    public void setToday(){
-        Calendar fechaE=new GregorianCalendar();
-        setFecha(fechaE.get(Calendar.DATE)+"/"+ fechaE.get(Calendar.MONTH)+"/"+fechaE.get(Calendar.YEAR));
-        fecha[1]+=1;
+    public void setToday() {
+        Calendar fechaE = new GregorianCalendar();
+        setFecha(fechaE.get(Calendar.DATE) + "/" + fechaE.get(Calendar.MONTH) + "/" + fechaE.get(Calendar.YEAR));
+        fecha[1] += 1;
     }
+
     /**
      * Introduce en la variable fecha la fecha desde un String
      *
@@ -92,7 +92,7 @@ public class Fecha {
                     for (int pos = 0; pos < fecha.length; pos++) {
                         fecha[pos] = 0;
                     }
-                } 
+                }
             }
         } catch (ArrayIndexOutOfBoundsException | NumberFormatException lessLenghtError) {//Error cuando faltan datos
             comprobacion = false;
@@ -100,44 +100,109 @@ public class Fecha {
         return comprobacion;
 
     }
-    
 
-    
-    
     /**
      * Comprueba si un año es bisiesto
+     *
      * @param year Se intruduce in int con el año a comprobar
-     * @return true=bisiesto    false=no bisiesto
+     * @return true=bisiesto false=no bisiesto
      */
-    public boolean bisiesto(int year){
-        boolean bisiesto=false;
-        if((year%4==0&&year%100!=0)||(year%100==0&&year%400!=0)){
-            bisiesto=true;
+    public boolean bisiesto(int year) {
+        boolean bisiesto = false;
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 100 == 0 && year % 400 != 0)) {
+            bisiesto = true;
         }
         return bisiesto;
     }
-    
-    public static int calcularEnios(Fecha fechaAlta, int cuantosEnio){
-        Fecha fechaHoy=new Fecha();
+
+    /** 
+     * Metodo que calcula los enios de una fecha hasta el dia de hoy.
+     * @param fechaAlta
+     * @param cuantosEnio
+     * @return 
+     */
+    public static int calcularEnios(Fecha fechaAlta, int cuantosEnio) {
+        Fecha fechaHoy = new Fecha();
         fechaHoy.setToday();
         int cantidad;
-        int annoCumplido=1;
-        if (fechaAlta.getMes()<fechaHoy.getMes()){
+        int annoCumplido = 1;
+        if (fechaAlta.getMes() < fechaHoy.getMes()) {
             annoCumplido--;
-        }else if(fechaAlta.getMes()==fechaHoy.getMes() && fechaAlta.getDia()<=fechaHoy.getDia()){
+        } else if (fechaAlta.getMes() == fechaHoy.getMes() && fechaAlta.getDia() <= fechaHoy.getDia()) {
             annoCumplido--;
         }
-        return (fechaHoy.getAnno()-fechaAlta.getAnno()-annoCumplido)/cuantosEnio;
-        
+        return (fechaHoy.getAnno() - fechaAlta.getAnno() - annoCumplido) / cuantosEnio;
+
     }
-    
-    public int getMes(){
+
+    /**
+     * Calcula el orden de la fecha
+     *
+     * @return retorna un Integer con el numero de orden
+     */
+    public int calcularOrden() {
+        int orden = 0;
+        int diasMesProvisional[] = diasMeses;
+        if (bisiesto(fecha[2])) {
+            diasMesProvisional[1] = 29;
+        }
+        for (int i = 0; i < fecha[1]; i++) {
+            orden += diasMesProvisional[i];
+        }
+        orden += fecha[1];
+        return orden;
+    }
+
+    /**
+     * A partir de los dias faltantes para el vencimiento saca la fecha del mismo
+     * @param diasVencimiento Introduce los dias para el vencimiento
+     * @return retorna la fecha de vencimiento
+     */
+    public Fecha calcularFechaVencimiento(int diasVencimiento) {
+        Fecha fechaHoy = new Fecha();
+        Fecha caducidad = new Fecha();
+        int diasMesProvisional[] = diasMeses;
+        fechaHoy.setToday();
+        int orden;
+        int diasAnno = 365;
+        orden = fechaHoy.calcularOrden();
+        if (bisiesto(fecha[2])) {
+            diasMesProvisional[1] = 29;
+            diasAnno = 366;
+        }
+        orden += diasVencimiento;
+        while (orden < diasAnno) {
+            fechaHoy.fecha[2] += 1;
+            orden -= diasAnno;
+
+            if (bisiesto(fechaHoy.fecha[2])) {
+                diasMesProvisional[1] = 29;
+                diasAnno = 366;
+            } else {
+                diasMesProvisional[1] = 28;
+                diasAnno = 365;
+            }
+        }
+        caducidad.fecha[2] = fechaHoy.fecha[2];
+        caducidad.fecha[1] = 1;
+        while (orden < diasMesProvisional[caducidad.fecha[1] - 1]) {
+            orden -= diasMesProvisional[caducidad.fecha[1] - 1];
+            caducidad.fecha[1]++;
+        }
+        caducidad.fecha[0] += orden;
+
+        return caducidad;
+    }
+
+    public int getMes() {
         return fecha[1];
     }
-    public int getDia(){
+
+    public int getDia() {
         return fecha[0];
     }
-    public int getAnno(){
+
+    public int getAnno() {
         return fecha[2];
     }
 }
